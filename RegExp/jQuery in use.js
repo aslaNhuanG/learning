415f9b4070c55dd26422bee1848e2c813a242543
aslan <1340985335@qq.com>
2014-11-17 16:04:24 +0800

    jquery

---
+++ RegExp/jQuery in use.js
@@ -0,0 +1,3 @@
+var regexp = [
+  /^gd[^r|f]fg$/ig
+];
--- javascript/1,jQuery core.js
+++ javascript/1,jQuery core.js
@@ -1,3 +1,8 @@
+/**
+ * jQuery 1.7.1 定义的基本框架
+ */
+
+// 显式什么undefined,确保是正确的undefined类型，也有利于压缩
 (function( window, undefined ){
 
   var jQuery = (function(){
@@ -13,14 +18,18 @@
           // ...
         }
       };
+
       // 指定fn为init.prototype的别名
       jQuery.fn.init.prototype = jQuery.fn;
+
       // 定义extend函数，方便扩展对象
       jQuery.extend = jQuery.fn.extend = function(){
         // ...
       };
+
       jQuery.extend({
         // 一堆静态属性和方法
+        // ...
       });
       return jQuery;
   })();
@@ -30,6 +39,14 @@
 })(window);
 
 /**
+
+ line 7
  Q:为什么要在构造函数jQuery内部用运算符new创建返回另外一个构造函数的实例
  A:jQuery利用内部运算符new创建并返回另一个构造函数(init)的实例，省去了创建jQuery对象的时候要调用new的语法，直接写jQuery()或$()即可。
+
+ line 17
+ Q:为什么要执行 jQuery.fn.init.prototype = jQuery.fn ？
+ A:用构造函数jQuery()的原型对象覆盖构造函数jQuery.fn.init的原型对象，从而使构造函数jQuery.fn.init的实例也能访问构造函数jQuery的原型方法
+  和属性，则最终返回给外部的init实例能能够使用jQuery的原型方法和属性
+
  */
